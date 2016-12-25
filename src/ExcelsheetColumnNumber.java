cacd799add228eef098d6133fc40f2b32f4428f6
Kavudhami <abiandabi92@gmail.com>
2016-12-25 12:09:17 -0800

    optimized

--- src/ExcelsheetColumnNumber.java
+++ src/ExcelsheetColumnNumber.java
@@ -11,13 +11,13 @@ public class ExcelsheetColumnNumber {
     public static int convertToTitle(String s) {
         if (s.length() == 1)
             return ((int) s.charAt(0)) - 64;
-        int ans = 0, i = 0;
-        int op1 = ((int) s.charAt(0)) - 64;
+        int i = 0;
+        int ans = ((int) s.charAt(0)) - 64;
         while (i < s.length() - 1) {
             int op2 = ((int) s.charAt(i + 1)) - 64;
-            ans = 26 * op1 + op2;
+            ans = 26 * ans + op2;
             i++;
-            op1 = ans;
+
         }
         return ans;
     }
