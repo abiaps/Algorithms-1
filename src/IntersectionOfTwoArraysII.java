11067f8d56446e3109b71ba809e122e939e4d8dc
Kavudhami <abiandabi92@gmail.com>
2016-12-27 18:06:54 -0800

    initial commit

--- src/IntersectionOfTwoArraysII.java
+++ src/IntersectionOfTwoArraysII.java
@@ -8,66 +8,32 @@ import java.util.Map;
 public class IntersectionOfTwoArraysII {
     public static void intersect(int[] nums1, int[] nums2) {
         ArrayList<Integer> res = new ArrayList<Integer>();
-        ArrayList<Integer> numsAL1 = new ArrayList<Integer>();
-        ArrayList<Integer> numsAL2 = new ArrayList<Integer>();
-        int flag = 0;
-
         HashMap<Integer,Integer> h1=new HashMap<Integer,Integer>();
         HashMap<Integer,Integer> h2=new HashMap<Integer,Integer>();
         for(int i=0;i<nums1.length;i++)
-        {
-            if(!h1.containsKey(nums1[i]))
-            h1.put(nums1[i],1);
-            if(h1.containsKey(nums1[i]))
-            {
-                h1.put(nums1[i],h1.get(nums1[i])+1);
-            }
-        }
+            if (!h1.containsKey(nums1[i])) h1.put(Math.abs(nums1[i]), 1);
+            else h1.put(Math.abs(nums1[i]), h1.get(Math.abs(nums1[i])) + 1);
+
         for(int j=0;j<nums2.length;j++)
-        {
-            if(!h2.containsKey(nums2[j]))
-            h2.put(nums2[j],1);
-            if(h2.containsKey(nums2[j]))
-            {
-                h2.put(nums2[j],h2.get(nums2[j])+1);
-            }
-        }
+            if (!h2.containsKey(nums2[j])) h2.put(Math.abs(nums2[j]), 1);
+            else h2.put(Math.abs(nums2[j]), h2.get(Math.abs(nums2[j])) + 1);
+
         System.out.println(h1);
         System.out.println(h2);
-        int dif=-1;
-        int min=0;
 
         for(Map.Entry<Integer,Integer> m1:h1.entrySet())
-        {
             for(Map.Entry<Integer,Integer> m2: h2.entrySet())
-            {
-                if(m1.getKey()==m2.getKey())
+                if (Math.abs(m1.getKey()) == Math.abs(m2.getKey()))
                 {
-                    dif=m1.getValue()-m2.getValue();
-                    if(dif==0)
-                    {
-                        for(int p=0;p<m1.getValue();p++)
-                        {
-                            res.add(m1.getKey());
-                        }
-                    }
-                    else if(dif>0)
-                    {
-                        min=Math.min(m1.getValue(),m2.getValue());
-                        for(int q=0;q<min;q++)
-                        {
-                            res.add(m1.getKey());
-                        }
-                    }
+                    int dif = (m1.getValue() < m2.getValue()) ? m1.getValue() : m2.getValue();
+                    for (int q = 0; q < dif; q++) res.add(m1.getKey());
                 }
-            }
-        }
         System.out.println(res);
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 1, 2, 1,3,4,6};
-        int[] nums2 = {2, 2,1,1,1,3,3,5};
+        int[] nums1 = {43, 85, 49, 2, 83, 2, 39, 99, 15, 70, 39, 27, 71, 3, 88, 5, 19, 5, 68, 34, 7, 41, 84, 2, 13, 85, 12, 54, 7, 9, 13, 19, 92};
+        int[] nums2 = {10, 8, 53, 63, 58, 83, 26, 10, 58, 3, 61, 56, 55, 38, 81, 29, 69, 55, 86, 23, 91, 44, 9, 98, 41, 48, 41, 16, 42, 72, 6, 4, 2, 81, 42, 84, 4, 13};
         intersect(nums1, nums2);
     }
 }
